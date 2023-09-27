90adae665c4f207e4ab52487e9d3cfd3d6a8864e
Divija Godse <92171879+divija14nd@users.noreply.github.com>
2023-09-27 12:15:33 +0530

    Sum Of Digits
    Program to find sum of all digits of a number entered by the user

---
+++ SumDigit.java
@@ -0,0 +1,23 @@
+package corejava;
+import java.util.Scanner;  
+
+public class SumDigit   
+{  
+public static void main(String args[])  
+{  
+	int num, digit, sum = 0;  
+	Scanner sc = new Scanner(System.in);  
+	System.out.print("Enter the number: ");  
+	num = sc.nextInt();  
+	
+	while(num > 0)  
+	{  
+		digit = num % 10;  
+		sum = sum + digit;  
+		num = num / 10;  
+	}  
+  
+	System.out.println("Sum of Digits: "+sum);  
+	sc.close();
+}  
+}  
