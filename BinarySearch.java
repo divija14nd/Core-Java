ec68341a8028f2928b67d67f5cbef509cc2701aa
Divija Godse <92171879+divija14nd@users.noreply.github.com>
2023-10-01 22:22:51 +0530

    Binary Search 1.1

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,10 +1,35 @@
 package corejava;
 
+/*  Write a program to perform binary search.  */
+
 public class BinarySearch {
+	
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int target = 12;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
 
-	}
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
+            if (arr[mid] == target) {
+                result = mid;
+                break;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+    }
 }
+
