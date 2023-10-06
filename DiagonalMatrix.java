c55798ce9c89ad8cea6a585b6d7c61a4688abfac
Divija Godse <92171879+divija14nd@users.noreply.github.com>
2023-10-06 18:37:02 +0530

    Update Single Loop

--- DiagonalMatrix.java
+++ DiagonalMatrix.java
@@ -16,17 +16,12 @@ public class DiagonalMatrix {
 				if ( i == j) {
 					res1 += matrix1[i][j];
 				}
-			}
-		}
-		
-		for (int i = matrix1.length-1; i >= 0; i--) {
-			for (int j = matrix1[i].length -1; j >= 0 ; j--) {
-				if ( i + j == (matrix1.length - 1)) {
+				if (i + j == (matrix1.length - 1)){
 					res2 += matrix1[i][j];
 				}
 			}
 		}
-		
+			
 		System.out.println("Sum of Diagonal 1 from top left to bottom right of the matrix is: " + res1 );
 		System.out.println("Sum of Diagonal 2 from top right to bottom left of the matrix is: " + res2 );
 	}
