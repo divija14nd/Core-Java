f322497c869718bef2e2d223ef8a8bae28943056
Divija Godse <92171879+divija14nd@users.noreply.github.com>
2023-10-06 18:54:06 +0530

    Updated for Jagged Array

--- OuterElements.java
+++ OuterElements.java
@@ -15,7 +15,7 @@ public class OuterElements {
 
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
-				if (i == 0 || i == matrix.length - 1 || j == 0 || j == matrix[0].length - 1) {
+				if (i == 0 || i == matrix.length - 1 || j == 0 || j == matrix[i].length - 1) {
 					System.out.print(matrix[i][j] + " ");
 				}
 			}
