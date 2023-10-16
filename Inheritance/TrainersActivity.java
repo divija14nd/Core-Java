e6ac179016b806a833fff2a5116555b28790105d
Divija Godse <92171879+divija14nd@users.noreply.github.com>
2023-10-16 12:00:37 +0530

    Update TrainersActivity.java

--- Inheritance/TrainersActivity.java
+++ Inheritance/TrainersActivity.java
@@ -4,7 +4,7 @@ public class TrainersActivity {
 
 	public static void main(String[] args) {
 		
-		  Employee employee = new Employee(1L, "Amar", "123 Main St", 5555555555L, 50000.0);
+	      Employee employee = new Employee(1L, "Amar", "123 Main St", 5555555555L, 50000.0);
 	      Manager manager = new Manager(2L, "Ram", "456 Corner St", 5555555556L, 65000.0);
 	      Trainee trainee = new Trainee(3L, "Shyam", "789 Circle St", 5555555557L, 45000.0);
 
